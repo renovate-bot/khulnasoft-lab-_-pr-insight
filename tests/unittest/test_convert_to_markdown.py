bc65323cbaa43aee71991d87bc68a69dae487ec0
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-08-13 02:47:56 +0600

    Update test_convert_to_markdown.py
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- tests/unittest/test_convert_to_markdown.py
+++ tests/unittest/test_convert_to_markdown.py
@@ -79,7 +79,7 @@ class TestBR:
         file_change_description = '- Imported `FilePatchInfo` and `EDIT_TYPE` from `pr_assistant.algo.types` instead of `pr_assistant.git_providers.git_provider`.'
         file_change_description_br = insert_br_after_x_chars(file_change_description)
         expected_output = ('<li>Imported <code>FilePatchInfo</code> and <code>EDIT_TYPE</code> from '
-                           '<code>pr_assistant.algo.types</code> <br>instead of <code>'
+                           '<code>pr_assistant.algo.types</code> instead <br>of '
                            '<code>pr_assistant.git_providers.git_provider</code>.')
         assert file_change_description_br == expected_output
         # print("-----")
